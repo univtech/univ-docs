a25038afcb529a4044e586aa9a5c9875cb0f227f
kisonhuang <916462938@qq.com>
2022-09-27 15:06:37 +0800

    简化主题页面和主题列表元素

--- content/extension/app/topic/topic-page.component.ts
+++ content/extension/app/topic/topic-page.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, Input, OnChanges, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
 
 import {htmlSafeByReview} from 'safevalues/restricted/reviewed';
 
@@ -27,7 +27,7 @@ import {TopicList} from './topic.model';
     selector: 'univ-topic-page',
     templateUrl: './topic-page.component.html',
 })
-export class TopicPageComponent implements AfterViewInit, OnChanges {
+export class TopicPageComponent implements AfterViewInit {
 
     // 页面标题
     @Input() title: string;
@@ -35,71 +35,18 @@ export class TopicPageComponent implements AfterViewInit, OnChanges {
     // 主题内容元素，引用`<div #topicContent>`
     @ViewChild('topicContent', {static: true}) topicContentElement: ElementRef<HTMLDivElement>;
 
-    // 主题内容
-    private _topicContent: TrustedHTML;
-
     // 主题列表
-    private _topicLists: TopicList[];
-
-    /**
-     * 获取主题内容
-     *
-     * @return 主题内容
-     */
-    get topicContent(): TrustedHTML {
-        return this._topicContent;
-    }
-
-    /**
-     * 设置主题内容
-     *
-     * @param topicContent 主题内容
-     */
-    set topicContent(topicContent: TrustedHTML) {
-        this._topicContent = topicContent;
-        this.parseTopicLists();
-    }
-
-    /**
-     * 获取主题列表
-     *
-     * @return 主题列表
-     */
-    get topicLists(): TopicList[] {
-        return this._topicLists;
-    }
-
-    /**
-     * 设置主题列表
-     *
-     * @param topicLists 主题列表
-     */
-    set topicLists(topicLists: TopicList[]) {
-        this._topicLists = topicLists;
-    }
+    topicLists: TopicList[];
 
     /**
      * 组件视图初始化完成之后的回调方法
      */
     ngAfterViewInit(): void {
         const topicContentElement = this.topicContentElement.nativeElement;
-        this.topicContent = htmlSafeByReview(topicContentElement.innerHTML, '^');
-        topicContentElement.textContent = '';
-    }
-
-    /**
-     * 数据绑定属性发生改变时的回调方法
-     */
-    ngOnChanges(): void {
-        this.parseTopicLists();
-    }
-
-    /**
-     * 解析主题列表
-     */
-    private parseTopicLists() {
-        const topicContent = this.topicContent?.toString() || '[]';
+        const topicContentHtml = htmlSafeByReview(topicContentElement.innerHTML, '^');
+        const topicContent = topicContentHtml?.toString() || '[]';
         this.topicLists = JSON.parse(`${topicContent}`);
+        topicContentElement.textContent = '';
     }
 
 }
--- src/content/extension/app/topic/topic-list.component.ts
+++ src/content/extension/app/topic/topic-list.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnChanges, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 import {htmlSafeByReview} from 'safevalues/restricted/reviewed';
 
@@ -22,76 +22,23 @@ import {Topic} from './topic.model';
     selector: 'univ-topic-list',
     templateUrl: './topic-list.component.html',
 })
-export class TopicListComponent implements AfterViewInit, OnChanges {
+export class TopicListComponent implements AfterViewInit {
 
     // 主题内容元素，引用`<div #topicContent>`
     @ViewChild('topicContent', {static: true}) topicContentElement: ElementRef<HTMLDivElement>;
 
-    // 主题内容
-    private _topicContent: TrustedHTML;
-
     // 主题列表
-    private _topics: Topic[];
-
-    /**
-     * 获取主题内容
-     *
-     * @return 主题内容
-     */
-    get topicContent(): TrustedHTML {
-        return this._topicContent;
-    }
-
-    /**
-     * 设置主题内容
-     *
-     * @param topicContent 主题内容
-     */
-    set topicContent(topicContent: TrustedHTML) {
-        this._topicContent = topicContent;
-        this.parseTopics();
-    }
-
-    /**
-     * 获取主题列表
-     *
-     * @return 主题列表
-     */
-    get topics(): Topic[] {
-        return this._topics;
-    }
-
-    /**
-     * 设置主题列表
-     *
-     * @param topics 主题列表
-     */
-    set topics(topics: Topic[]) {
-        this._topics = topics;
-    }
+    topics: Topic[];
 
     /**
      * 组件视图初始化完成之后的回调方法
      */
     ngAfterViewInit(): void {
         const topicContentElement = this.topicContentElement.nativeElement;
-        this.topicContent = htmlSafeByReview(topicContentElement.innerHTML, '^');
-        topicContentElement.textContent = '';
-    }
-
-    /**
-     * 数据绑定属性发生改变时的回调方法
-     */
-    ngOnChanges(): void {
-        this.parseTopics();
-    }
-
-    /**
-     * 解析主题列表
-     */
-    private parseTopics() {
-        const topicContent = this.topicContent?.toString() || '[]';
+        const topicContentHtml = htmlSafeByReview(topicContentElement.innerHTML, '^');
+        const topicContent = topicContentHtml?.toString() || '[]';
         this.topics = JSON.parse(`${topicContent}`);
+        topicContentElement.textContent = '';
     }
 
 }
